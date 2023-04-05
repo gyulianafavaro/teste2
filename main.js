3971f20a3f3ae2a110bc423e93546effd379a0bb
gyulianafavaro <129889861+gyulianafavaro@users.noreply.github.com>
2023-04-05 12:05:05 -0300

    alteração

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("click" , showcolor );
 
 document. getElementById("botao").addEventListener("click" , exibenome );
 
+document. getElementById("btn-soma").addEventListener("click" , soma );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -13,5 +15,10 @@ function exibenome()
     document.getElementById("mensagem").innerHTML = "Meu nome é:" + usuario;
     document.getElementById("nome").value = "";
 }
+
 function soma()
-a = required(), b = required() { return a + b; } console. log(soma(1, 2));
+{
+    let numero1 = document.getElementById("num1").value;
+    let numero2 = document.getElementById("num2").value;
+    document.getElementById("resultado1").innerHTML = parseFloat(numero1) + parseFloat(numero2);
+}
