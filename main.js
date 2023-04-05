675e2f1d15578d59926639e731ef1e9d35891f28
gyulianafavaro <129889861+gyulianafavaro@users.noreply.github.com>
2023-04-05 12:31:41 -0300

    sinais

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document. getElementById("btn-dividir").addEventListener("click" , dividir );
 document. getElementById("btn-multiplicar").addEventListener("click" , multiplicar);
 
 document. getElementById("btn-subtrair").addEventListener("click" , subtrair );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -30,20 +31,20 @@ function soma()
 
 function dividir()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
+    let numero1 = document.getElementById("num3").value;
+    let numero2 = document.getElementById("num4").value;
     document.getElementById("resultado2").innerHTML = parseFloat(numero1) / parseFloat(numero2);
 }
 function multiplicar()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
-    document.getElementById("resultado3").innerHTML = parseFloat(numero1) x parseFloat(numero2);
+    let numero1 = document.getElementById("num5").value;
+    let numero2 = document.getElementById("num6").value;
+    document.getElementById("resultado3").innerHTML = parseFloat(numero1) * parseFloat(numero2);
 }
 function subtrair()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
+    let numero1 = document.getElementById("num7").value;
+    let numero2 = document.getElementById("num8").value;
     document.getElementById("resultado4").innerHTML = parseFloat(numero1) - parseFloat(numero2);
 }
 
