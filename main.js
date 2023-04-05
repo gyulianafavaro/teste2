eae0eb2af2df3b33db91feb12f2a57f4005a4475
gyulianafavaro <129889861+gyulianafavaro@users.noreply.github.com>
2023-04-05 11:51:52 -0300

    alteração soma

--- main.js
+++ main.js
@@ -13,3 +13,4 @@ function exibenome()
     document.getElementById("mensagem").innerHTML = "Meu nome é:" + usuario;
     document.getElementById("nome").value = "";
 }
+function soma(a = required(), b = required()) { return a + b; } console. log(soma(1, 2)); // 3 console.
